ae35a2d0a89232ed4c7acc81e25e1ca5fa665585
Bruno Flaven <bflaven@gmail.com>
2015-09-28 21:07:44 +0200

    Multilingualism, Symfony, App - Generate a translation .xml or .xliff from a csv file from a google doc

---
+++ convert-csv-2-xml-or-xliff/README.md
@@ -0,0 +1,45 @@
+
+# Convert .csv to .xml or .xliff
+=============================================
+
+This very simple script, using PHP command line, enables to convert a csv file into a .xliff file used in Symfony or into a .xml file generally used in mobile development.
+
+
+
+## Installation
+---------------------
+Download the directory convert-csv-2-xml-or-xliff.
+
+
+
+## Usage
+--------------
+
+NOTE: I have left a sample data in csv folder and in the dest folder. The dest folder is where the .xml or .xliff will be outputted.
+
+1. **Add your own CSV** with your own columns eg TYPE,STRING,FR,EN
+2. **Define the configuration you want.** See // ---- File outut. You can choose between .xml or .xliff. Feel free to add your template in the folder <b>template</b>
+3. **Define the parameters for reading your CSV.** See // ---- Reading the CSV
+4. **Open the console and go the folder**
+```
+cd /[your-path]/convert-csv-2-xml-or-xliff/
+```
+
+5. **Run the following command**
+```
+php convert-csv-2-xml-or-xliff.php
+```
+
+
+## For more information
+------------------------------------
+Feel free to check in French some articles @
+[Flaven.net](http://flaven.fr//)
+
+
+
+
+
+
+
+
---
+++ convert-csv-2-xml-or-xliff/convert-csv-2-xml-or-xliff.php
@@ -0,0 +1,91 @@
+<?php
+// CONFIGURATION
+/* The source file */
+define('SOURCE_EXTENSION', 'csv');
+
+
+// ---- File outut
+/* For symfony, do not put full extension, just put what is behind the dot (.) eg .xliff it becomes xliff */
+/*
+define('TARGET_EXTENSION', 'xliff');
+define('TARGET_TEMPLATE', __DIR__.'/template/xliff_sf/template.xml');
+define('TARGET_SUB_TEMPLATE', __DIR__.'/template/xliff_sf/transunit.xml');
+*/
+
+/* For mobile, do not put full extension, just put what is behind the dot (.) eg .xml it becomes xml */
+define('TARGET_EXTENSION', 'xml');
+define('TARGET_TEMPLATE', __DIR__.'/template/xml_mobile/template.xml');
+define('TARGET_SUB_TEMPLATE', __DIR__.'/template/xml_mobile/transunit.xml');
+
+
+
+
+// ---- Reading the CSV
+define('SOURCE_DELIMITER', ',');
+define('SOURCE_KEY_COL', 1);
+define('SOURCE_VALUE_COL', 3);
+
+// ---- Other values
+define('SOURCE_DIR', __DIR__.'/'.SOURCE_EXTENSION);
+define('TARGET_DIR', __DIR__.'/dest');
+
+// FUNCTIONS
+function csv2arr($sourceFile, $delimiter) {
+    if (($handle = fopen(SOURCE_DIR.'/'.$sourceFile, "r")) !== FALSE) {
+        $i = 0; 
+        while (($lineArray = fgetcsv($handle, 4000, $delimiter)) !== FALSE) {
+            for ($j=0; $j<count($lineArray); $j++) {
+                $arr[$i][$j] = $lineArray[$j];
+            }
+            $i++;
+        }
+        fclose($handle);
+    }
+    return $arr;
+}
+
+function arr2xml($arr, $sourceFile, $transunitTemplate, $transTemplate) {
+    $transunits = array();
+    foreach($arr as $line => $data) {
+        if ($arr[$line][SOURCE_KEY_COL] == ''/* || $arr[$line][SOURCE_VALUE_COL] == ''*/) {
+            continue;
+        }
+        $subtrans = array(
+            '{ID}'      => sha1(uniqid()),
+            '{RESNAME}' => $arr[$line][SOURCE_KEY_COL],
+            '{KEY}'     => $arr[$line][SOURCE_KEY_COL],
+            '{VALUE}'   => $arr[$line][SOURCE_VALUE_COL]
+        );
+        $S = $T = array();
+        foreach($subtrans as $s => $t) {
+            $S[] = $s;
+            $T[] = $t;
+        }
+        $transunits[] = str_replace($S, $T, $transunitTemplate);
+    }
+    $translated = str_replace('{TRANSUNIT}', implode("\n", $transunits), $transTemplate);
+    //print_r($translated); die();
+    file_put_contents(TARGET_DIR.'/'.pathinfo($sourceFile, PATHINFO_FILENAME).'.'.TARGET_EXTENSION, $translated);
+}
+
+function loopSourceDir($sourceFiles) {
+    $transunitTemplate = file_get_contents(TARGET_SUB_TEMPLATE);
+    $transTemplate = file_get_contents(TARGET_TEMPLATE);
+    foreach($sourceFiles as $sourceFile) {
+        if (!is_file(SOURCE_DIR.'/'.$sourceFile)) {
+            continue;
+        }
+        $sourceFileExtension = pathinfo(SOURCE_DIR.'/'.$sourceFile, PATHINFO_EXTENSION);
+        if ($sourceFileExtension != SOURCE_EXTENSION) {
+            continue;
+        }
+        $arr = csv2arr($sourceFile, SOURCE_DELIMITER);
+        arr2xml($arr, $sourceFile, $transunitTemplate, $transTemplate);
+    }
+}
+
+// EXECUTION
+$sourceFiles = scandir(SOURCE_DIR);
+loopSourceDir($sourceFiles);
+
+?>
