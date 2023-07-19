b10725392c5a78e6a51ed39585176a05ff9e0bcb
Bruno Flaven <bflaven@gmail.com>
2023-07-19 11:41:29 +0200

    update files

--- ai_chatgpt_prompts/project_1_python_documentation_chatgpt_api/005_project_1_python_documentation_default_summarize_chatgpt_api.py
+++ ai_chatgpt_prompts/project_1_python_documentation_chatgpt_api/005_project_1_python_documentation_default_summarize_chatgpt_api.py
@@ -48,9 +48,6 @@ openai.organization = OPENAI_ORGANIZATION
 # PAID ONE DO NOT DISPLAY
 openai.api_key = OPENAI_API_KEY
 
-
-
-
 response = openai.Completion.create(
     model="text-davinci-003",
     prompt="Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.",
